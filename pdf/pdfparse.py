7971e40f50ab21bc4d097ffccba0b076f0ab0c76
Iraqez <vladimir.gumenyuk@gmail.com>
2016-08-27 14:23:50 +0300

    2

--- pdf/pdfparse.py
+++ pdf/pdfparse.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from pdfminer import pdfparser
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+
+# Open a PDF document.
+fp = open('/home/iraqez/aik_yarm.pdf', 'rb')
+parser = PDFParser(fp)
+document = PDFDocument(parser)
+
+# Get the outlines of the document.
+outlines = document.get_outlines()
+print outlines
+# for (level,title,dest,a,se) in outlines:
+#     print (level, title)
 
